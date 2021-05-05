02893913b618dc1b552e7a4257305d1103040c30
Leonardo dos Santos <leodoima@hotmail.com>
2021-05-05 19:28:31 -0300

    Criando animaçao para toasts

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,18 +1,18 @@
-import React, { createContext, useContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useContext, useState } from 'react';
 import { uuid } from 'uuidv4';
 
 import ToastContainer from '../components/ToastContainer';
 
+export interface ToastMessage {
+  id: string;
+  type?: 'success' | 'error' | 'info';
+  title: string;
+  description?: string;
+}
+
 interface ToastContextData {
   addToast(message: Omit<ToastMessage, 'id'>): void;
   removeToast(id: string): void;
-}
-
-export interface ToastMessage {
-  id: string;
-  type: 'success' | 'error' | 'info';
-  title: string;
-  description?: string;
 }
 
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
@@ -20,7 +20,6 @@ const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 const ToastProvider: React.FC = ({ children }) => {
   const [messages, setMessages] = useState<ToastMessage[]>([]);
 
-  // Parâmetro é toda a interface do ToastMessage exceto o campo ID que será alimentado internamente
   const addToast = useCallback(
     ({ type, title, description }: Omit<ToastMessage, 'id'>) => {
       const id = uuid();
@@ -32,14 +31,12 @@ const ToastProvider: React.FC = ({ children }) => {
         description,
       };
 
-      // Recebe as mensagens antigas e também as novas toasts
       setMessages(state => [...state, toast]);
     },
     [],
   );
 
   const removeToast = useCallback((id: string) => {
-    // Aplica filtro para que o tost removido não seja exibido
     setMessages(state => state.filter(message => message.id !== id));
   }, []);
 
@@ -57,6 +54,7 @@ function useToast(): ToastContextData {
   if (!context) {
     throw new Error('useToast must be used within a ToastProvider');
   }
+
   return context;
 }
 
